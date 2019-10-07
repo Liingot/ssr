519febfd00b63e08080cad9f8df6b9e93cde3118
Liingot <1364434056@qq.com>
2019-10-07 20:31:41 +0800

    第一次提交

---
+++ src/components/banner/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <swiper
+      class="cont"
+      @change="switchItem($event)"
+      :current="currentTab"
+      circular="true"
+      indicator-dots="true"
+      skip-hidden-item-layout="true"
+    >
+      <swiper-item v-for="(item,index) in bannerList" :key="index" @click="look(item)">
+        <div class="banner">{{item}}</div>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    bannerList: {
+      type: Array,
+      defalut: () => {
+        return [1, 2, 3];
+      }
+    }
+  },
+  data() {
+    return {
+      currentTab: 0 //当前所在滑块的index
+    };
+  },
+  methods: {
+    switchItem(v) {
+      console.log(v);
+    },
+    look(v){
+        this.$emit('bannerLook',v)
+    }
+  }
+};
+</script>
+<style scoped>
+.banner {
+  width: 100%;
+  height: 355rpx;
+  background: rosybrown;
+}
+.banner img {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <index-banner :bannerList="bannerList" @bannerLook="bannerLook"></index-banner>
+    <section class="main">
+      <div class="information">
+        <div class="informationLeft">
+          <div class="meetingLogo"></div>
+          <h3 class="meetingH3">会议信息</h3>
+        </div>
+        <div class="informationRight">
+          <span>分享给好友</span>
+          <div class="share"></div>
+        </div>
+      </div>
+      <div class="list">
+        <section class="listChildren" v-for="(item,index) in 10" :key="index">
+          <div class="listPhoto"></div>
+          <div class="listText">
+            <div class="listTextTop">123123213123123123123123</div>
+            <div class="listTextBottom">
+              <span>10月30日 周三</span>
+              <div class="info">
+                <div class="infoLogo"></div>
+                <span>北京</span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+import indexBanner from "../../components/banner"; //banner
+export default {
+  components: { indexBanner },
+  data() {
+    return {
+      bannerList: [1, 2, 3]
+    };
+  },
+  methods: {
+    bannerLook(v) {
+      //点击banner的回调
+    }
+  }
+};
+</script>
+<style scoped>
+.main {
+  padding: 30rpx 20rpx;
+  box-sizing: border-box;
+  background: #f1f1f1;
+}
+.information {
+  background: white;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 30rpx 15rpx;
+  box-sizing: border-box;
+}
+.informationLeft,
+.informationRight {
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+}
+.meetingLogo {
+  width: 38rpx;
+  height: 38rpx;
+  background: royalblue;
+}
+.meetingLogo img {
+  width: 100%;
+  height: 100%;
+}
+.meetingH3 {
+  font-size: 35rpx;
+  font-weight: 600;
+  margin-left: 10rpx;
+}
+.informationRight {
+  height: 56rpx;
+  border-radius: 15px;
+  border: 1px solid #ccc;
+  font-size: 30rpx;
+  color: #0070cc;
+  text-align: center;
+  padding: 0 25rpx;
+  box-sizing: border-box;
+  justify-content: space-between;
+}
+.share {
+  width: 26rpx;
+  height: 26rpx;
+  background: red;
+  margin-left: 15rpx;
+}
+.share img {
+  width: 100%;
+  height: 100%;
+}
+.list {
+  margin-top: 5rpx;
+}
+.listChildren {
+  background: white;
+  margin-bottom: 10rpx;
+  padding: 30rpx 15rpx;
+  display: flex;
+}
+.listPhoto {
+  width: 230rpx;
+  height: 155rpx;
+  border-radius: 5rpx;
+  background: yellowgreen;
+  margin-right: 30rpx;
+}
+.listPhoto img {
+  width: 100%;
+  height: 100%;
+}
+.listTextTop {
+  font-size: 28rpx;
+  font-weight: 600;
+  padding-bottom: 45rpx;
+  box-sizing: border-box;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+  word-break: break-all;
+}
+.listTextBottom {
+  font-size: 20rpx;
+  display: flex;
+  justify-content: space-between;
+  color: #999999;
+}
+.info {
+  display: flex;
+}
+.infoLogo {
+  width: 28rpx;
+  height: 34rpx;
+  margin-right: 5rpx;
+  background: tan;
+}
+.infoLogo img {
+  width: 100%;
+  height: 100%;
+}
+</style>
