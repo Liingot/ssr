e9e3c1170acc4fcdfeb19fd8278b00e096078ffd
Liingot <1364434056@qq.com>
2019-10-19 23:52:52 +0800

    update

--- src/pages/moduleMy/myTicket/index.vue
+++ src/pages/moduleMy/myTicket/index.vue
@@ -41,7 +41,7 @@ export default {
     return {
       use: 1, //未使用数量
       has: 2, //已使用数量
-      addborder: false,
+      addborder: true,
       lastPage: 1,
       myTicketList: [],
       currentPage: 1,
@@ -51,7 +51,7 @@ export default {
   onUnload() {
     //页面卸载
     this.myTicketList = [];
-    this.addborder = false;
+    this.addborder = true;
   },
   mounted() {
     this.url = this.domains;
@@ -62,7 +62,7 @@ export default {
   },
   methods: {
     // status = 0(未使用) 1(已使用)
-    init(status = 1, page = 1) {
+    init(status = 0, page = 1) {
       this.axios
         .post({
           url: "/api/personal/ticketList",
@@ -93,7 +93,7 @@ export default {
         this.addborder = true;
         this.myTicketList = [];
         this.currentPage = 1;
-        this.init(0);
+        this.init();
       } else {
         this.addborder = false;
         this.myTicketList = [];
